cc15641e6b0e2b1164b588f5ef2df8a51a276797
Maxim7972 <120516216+Maxim7972@users.noreply.github.com>
2025-06-26 14:18:13 +0300

    маша

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,7 +4,7 @@ package org.example;
     public class App {
         public static void main(String[] args) {
             Tester.printStaticInfo();
-            Tester tester = new Tester("Ромка", "Татур", 5, "London is a capital of the Great britan", 5000.0);
+            Tester tester = new Tester("Маша", "Юнакова", 5, "London is a capital of the Great britan", 5000.0);
             tester.printInfo(true);
         }
     }
